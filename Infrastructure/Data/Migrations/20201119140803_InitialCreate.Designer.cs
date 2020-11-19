fad063cb78242c2e0393697466713c7e1514f9fd
sivakumar <52204776+siva4039@users.noreply.github.com>
2020-11-19 20:10:22 +0530

    recommit

--- Infrastructure/Data/Migrations/20201119140803_InitialCreate.Designer.cs
+++ Infrastructure/Data/Migrations/20201119140803_InitialCreate.Designer.cs
@@ -2,18 +2,18 @@
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20201119140803_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
-
-
-
+#pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "3.1.10");
 
