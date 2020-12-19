908fb25c8969b7bab86bdc4c8d5459f3e7c3a5b6
sivakumar <52204776+siva4039@users.noreply.github.com>
2020-12-19 15:42:14 +0530

    sort_filter_featuresAdded

--- Core/Specifications/BaseSpecifications.cs
+++ Core/Specifications/BaseSpecifications.cs
@@ -12,16 +12,29 @@ namespace Core.Specifications
 
         public BaseSpecifications(Expression<Func<T, bool>> criteria)
         {
-            Criteria = criteria;
+            Criteria = criteria;;
         }
 
         public Expression<Func<T, bool>> Criteria {get; }
 
         public List<Expression<Func<T, object>>> Includes {get; } = new List<Expression<Func<T, object>>>();
 
+        public Expression<Func<T, object>> OrderBy {get; private set;}
+
+        public Expression<Func<T, object>> OrderByDecending {get; private set;}
+
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
             Includes.Add(includeExpression);
+        }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+        protected void AddOrderByDecending(Expression<Func<T, object>> orderByDecendingExpression)
+        {
+            OrderByDecending = orderByDecendingExpression;
         }
     }
 }
--- Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
@@ -6,10 +6,37 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandSpecification : BaseSpecifications<Product>
     {
-        public ProductsWithTypesAndBrandSpecification()
+        public ProductsWithTypesAndBrandSpecification(string sort, int? brandId, int? typeId)
+        :base(x=> 
+        (
+            (!brandId.HasValue || x.ProductBrandId == brandId) && (!typeId.HasValue || x.ProductTypeId == typeId)
+
+        ))
         {
             AddInclude(x => x.ProductType);
             AddInclude(x=> x.ProductBrand);
+            AddOrderBy(x => x.Name);
+
+            if(!string.IsNullOrEmpty(sort))
+            {
+                switch(sort)
+                {
+                    case "priceAsc" :
+                            AddOrderBy(p => p.Price);
+                            break;
+
+                    case "priceDsc" :
+                            AddOrderByDecending(p => p.Price);
+                            break;
+
+                    default :
+                            AddOrderBy(n=>n.Name);
+                            break;
+                }
+            }
+
+
+
         }
 
         public ProductsWithTypesAndBrandSpecification(int id) : base(x => x.Id == id)
