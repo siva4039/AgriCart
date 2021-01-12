6a2cf4394fe6ffeac40a60cca39d5d2369a45f76
sivakumar <52204776+siva4039@users.noreply.github.com>
2021-01-12 20:04:12 +0530

    Identity_validation_added

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -95,6 +95,13 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if(CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new []{
+                    "Email is in use"
+
+                }});
+            }
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -9,11 +9,33 @@ namespace API.Extensions
         public static IServiceCollection AddSwaggerDocumentaion(this IServiceCollection services)
         {
             services.AddSwaggerGen(c =>
-            {c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
                 {
-                    Title = "AgriCart API"
+                    Title = "AgriCart API",
+                    Version = "v1"
                 });
-            });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer",securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement {{securitySchema, new[]
+                {"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirement);
+
+            }
+            );
             return services;
         }
 
